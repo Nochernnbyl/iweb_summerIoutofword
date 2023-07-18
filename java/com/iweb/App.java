a18f6c002bab5b9fd4d74fbae169cf3c95bebba4
daosir <j2717927552@163.com>
2023-07-18 17:13:47 +0800

    这里面封装了excel的导入功能，写入了对Product表的insertAll功能（Product设置了自增） 再送你一个账户充值功能

--- java/com/iweb/App.java
+++ java/com/iweb/App.java
@@ -1,5 +1,11 @@
 package com.iweb;
 
+import com.iweb.service.ProductImport;
+import com.iweb.service.productImportImpl.ImportUtil;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ProductImport productImport = new ImportUtil();
+        productImport.productImport(new File(""), XSSFWorkbook.class);
     }
 }
